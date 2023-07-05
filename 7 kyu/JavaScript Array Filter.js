533c5837e5a09daf580674f6fa1bd4b39e77377a
BBalazs37 <ballard37@gmail.com>
2023-07-05 10:39:31 +0200

    easy daily

---
+++ 7 kyu/JavaScript Array Filter.js
@@ -0,0 +1,10 @@
+//https://www.codewars.com/kata/514a6336889283a3d2000001/
+
+/*
+JavaScript Arrays support a filter function (starting in JavaScript 1.6). Use the filter functionality to complete the function given.
+getEvenNumbers([2,4,5,6]) // should == [2,4,6]
+*/
+
+function getEvenNumbers(numbersArray){
+  return numbersArray.filter(a => a % 2 == 0)
+}
