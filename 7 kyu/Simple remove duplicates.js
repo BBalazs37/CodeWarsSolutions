a3a95cbdc964a18f1900128a422d945020d6be27
BBalazs37 <ballard37@gmail.com>
2023-08-12 09:11:32 +0200

    daily not so simple dublicates

---
+++ 7 kyu/Simple remove duplicates.js
@@ -0,0 +1,17 @@
+//https://www.codewars.com/kata/5ba38ba180824a86850000f7/
+/*
+Remove the duplicates from a list of integers, keeping the last ( rightmost ) occurrence of each element.
+
+Example:
+For input: [3, 4, 4, 3, 6, 3]
+
+remove the 3 at index 0
+remove the 4 at index 1
+remove the 3 at index 3
+Expected output: [4, 6, 3]
+*/
+
+function solve(arr) {
+  arr.reverse()
+  return arr.filter((item, pos) => arr.indexOf(item) == pos).reverse()
+}
