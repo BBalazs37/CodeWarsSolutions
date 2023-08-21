e1d6231a6d7cf18d3368a878e5bf332818aeb7ba
BBalazs37 <ballard37@gmail.com>
2023-08-21 09:10:43 +0200

    daily summation

---
+++ 7 kyu/Sum of all arguments.js
@@ -0,0 +1,12 @@
+//https://www.codewars.com/kata/540c33513b6532cd58000259/
+/*
+Write a function that finds the sum of all its arguments.
+
+sum(1, 2, 3) // => 6
+sum(8, 2) // => 10
+sum(1, 2, 3, 4, 5) // => 15
+*/
+
+function sum() {
+  return Array.from(arguments).reduce((a,b) => a + b, 0)
+}
