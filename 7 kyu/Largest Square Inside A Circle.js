361ac77700fbb16239028f5debc1d358dd663f36
BBalazs37 <ballard37@gmail.com>
2023-09-25 10:16:51 +0200

    daily square inside a circle

---
+++ 7 kyu/Largest Square Inside A Circle.js
@@ -0,0 +1,5 @@
+//https://www.codewars.com/kata/5887a6fe0cfe64850800161c/
+/*
+Determine the area of the largest square that can fit inside a circle with radius r.
+*/
+let areaLargestSquare = r => r**2*2
