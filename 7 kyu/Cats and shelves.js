631fbe2dbc188927121e6bf14a2c6dd327ab25b4
BBalazs37 <ballard37@gmail.com>
2023-11-10 09:05:13 +0100

    daily cats and shelves :3

---
+++ 7 kyu/Cats and shelves.js
@@ -0,0 +1,43 @@
+//https://www.codewars.com/kata/62c93765cef6f10030dfa92b/
+/*
+An infinite number of shelves are arranged one above the other in a staggered fashion.
+The cat can jump either one or three shelves at a time: from shelf i to shelf i+1 or i+3 (the cat cannot climb on the shelf directly above its head), according to the illustration:
+
+                 ┌────────┐
+                 │-6------│
+                 └────────┘
+┌────────┐       
+│------5-│        
+└────────┘  ┌─────► OK!
+            │    ┌────────┐
+            │    │-4------│
+            │    └────────┘
+┌────────┐  │
+│------3-│  │     
+BANG!────┘  ├─────► OK! 
+  ▲  |\_/|  │    ┌────────┐
+  │ ("^-^)  │    │-2------│
+  │ )   (   │    └────────┘
+┌─┴─┴───┴┬──┘
+│------1-│
+└────────┘
+Input
+Start and finish shelf numbers (always positive integers, finish no smaller than start)
+
+Task
+Find the minimum number of jumps to go from start to finish
+*/
+
+function solution(start, finish) 
+{
+  let jumps = 0;
+  while(start < finish){
+    if(finish-start >=3){
+      start += 3;
+    } else {
+      start += 1;
+    }
+    jumps++;
+  }
+  return jumps;
+}
