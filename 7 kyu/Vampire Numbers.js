05886829570f9ec281c33868ebf7cd925e1d6eba
BBalazs37 <ballard37@gmail.com>
2024-02-09 10:08:22 +0100

    daily vampire numbers 🧛‍♂️

---
+++ 7 kyu/Vampire Numbers.js
@@ -0,0 +1,21 @@
+//https://www.codewars.com/kata/54d418bd099d650fa000032d/
+/*
+Our loose definition of Vampire Numbers can be described as follows:
+
+6 * 21 = 126
+# 6 and 21 would be valid 'fangs' for a vampire number as the 
+# digits 6, 1, and 2 are present in both the product and multiplicands
+
+10 * 11 = 110
+# 110 is not a vampire number since there are three 1's in the
+# multiplicands, but only two 1's in the product
+Create a function that can receive two 'fangs' and determine if the product of the two is a valid vampire number.
+*/
+
+function vampireTest(a, b) {
+  let strFangs = String(a) + String(b);
+  let strProduct = String(a*b);
+  strFangs = strFangs.split('').sort().join('');
+  strProduct = strProduct.split('').sort().join('');
+  return strFangs == strProduct;
+}
