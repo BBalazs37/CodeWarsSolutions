351573db3af5ab74ee1d34a824df4076a248a64a
BBalazs37 <ballard37@gmail.com>
2024-02-13 09:54:08 +0100

    daily smalls

---
+++ 7 kyu/How many are smaller than me.js
@@ -0,0 +1,12 @@
+//https://www.codewars.com/kata/56a1c074f87bc2201200002e/
+/*
+Write a function that given, an array arr, returns an array containing at each index i the amount of numbers that are smaller than arr[i] to the right.
+
+For example:
+
+* Input [5, 4, 3, 2, 1] => Output [4, 3, 2, 1, 0]
+* Input [1, 2, 0] => Output [1, 1, 0]
+*/
+function smaller(nums) { 
+  return nums.map((a,i,arr)=>arr.slice(i).filter(b=> b < a).length)
+}
