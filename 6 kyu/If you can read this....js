5c71626fb4b3870db5a6e5458e158e00fd495e2f
BBalazs37 <ballard37@gmail.com>
2024-03-01 09:52:03 +0100

    Delta Alfa India Lima Yankee

---
+++ 6 kyu/If you can read this....js
@@ -0,0 +1,28 @@
+//https://www.codewars.com/kata/586538146b56991861000293/
+/*
+Task
+You'll have to translate a string to Pilot's alphabet (NATO phonetic alphabet).
+
+Input:
+If, you can read?
+
+Output:
+India Foxtrot , Yankee Oscar Uniform Charlie Alfa November Romeo Echo Alfa Delta ?
+
+Note:
+There is a preloaded dictionary that you can use, named NATO. It uses uppercase keys, e.g. NATO['A'] is "Alfa". See comments in the initial code to see how to access it in your language.
+The set of used punctuation is ,.!?.
+Punctuation should be kept in your return string, but spaces should not.
+Xray should not have a dash within.
+Every word and punctuation mark should be seperated by a space ' '.
+There should be no trailing whitespace
+*/
+
+function toNato(words) {
+  return words
+    .toUpperCase()
+    .split('')
+    .filter(a=>a !== ' ')
+    .map(a=>NATO[a] || a)
+    .join(' ');
+}
