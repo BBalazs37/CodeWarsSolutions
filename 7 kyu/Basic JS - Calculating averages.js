11a1f987ab700708843b2dd5ecb6eed8c1a52ade
BBalazs37 <ballard37@gmail.com>
2024-03-06 09:54:27 +0100

    daily averages

---
+++ 7 kyu/Basic JS - Calculating averages.js
@@ -0,0 +1,19 @@
+//https://www.codewars.com/kata/529f32794a6db5d32a00071f/
+/*
+Let's build a calculator that can calculate the average for an arbitrary number of arguments.
+
+The test expects you to provide a Calculator object with an average method:
+
+Calculator.average()
+The test also expects that when you pass no arguments, it returns 0. The arguments are expected to be integers.
+
+It expects Calculator.average(3,4,5) to return 4.
+*/
+
+var Calculator = {
+ average: function() {
+   let args = Array.from(arguments);
+   if(args.length == 0) return 0;
+   return args.reduce((a,b)=>a+b,0) / args.length;
+ }
+};
