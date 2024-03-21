fb6f9e31bd85aa7b03e390397b8c571331962cb2
BBalazs37 <ballard37@gmail.com>
2024-03-21 09:38:11 +0100

    daily wait a minute this isn't a coding challenge. this is a math challenge

---
+++ 7 kyu/Perimeter sequence.js
@@ -0,0 +1,15 @@
+//https://www.codewars.com/kata/589519d1f0902e01af000054/
+/*
+Perimeter sequence
+The first three stages of a sequence are shown.
+
+blocks
+
+The blocksize is a by a and a ≥ 1.
+
+What is the perimeter of the nth shape in the sequence (n ≥ 1) ?
+*/
+
+function perimeterSequence(a,n) {
+  return n*a*4;
+}
